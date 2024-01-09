e4423d8bfa591dcfafe01fb52282a988e4cd1455
vikas Semwal <vikas.semwal1994@gmail.com>
2024-01-10 00:22:42 +0530

    Local Pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [slots, setSlots] = useState([]);
+  const [pageData, setPageData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummy.restapiexample.com/api/v1/employees')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log('response', data.data);
+        convertDataInPagination(data.data)
+      })
+      .catch((error) => {
+        console.error('There was a problem fetching the data:', error);
+      });
+  }, []);
+
+  const convertDataInPagination = (employees) => {
+    let arrayOfPages = []
+    
+    let lengthOfArray = employees.length
+    let slots = Math.ceil(lengthOfArray/10)
+    let startingIndex = 0
+
+    for(let i = 0; i<slots; i++){
+      let slot = employees.slice(startingIndex, startingIndex+10)
+      startingIndex = startingIndex+10
+      arrayOfPages.push(slot)
+    }
+    console.log("pages array", arrayOfPages)
+    setSlots(arrayOfPages)
+    setPageData(arrayOfPages[0])
+  }
+
+  const convertDataWithOtherAproach = (employees)  => {
+    
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ul>
+     {pageData.map((item)=>{
+      return <li>
+        <p>{item.employee_name}</p>
+      </li>
+     })}
+     </ul>
+     {slots.map((item, index)=>{
+      return <button onClick={()=>{
+        console.log("item", item)
+        setPageData(item)
+      }}>{index+1}</button>
+     })}
+     
     </div>
   );
 }
